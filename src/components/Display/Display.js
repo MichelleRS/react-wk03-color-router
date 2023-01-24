82d00669014f07ebdb1669955d73b9c6d0221947
Michelle Ross <miross629@gmail.com>
2023-01-24 12:00:27 -0800

    RGB color values show in Display; TODO: add style

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './Display.css';
 
 export default function Display() {
   const { r, g, b } = useParams();
   return (
     <section style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
-      <p>Choose your color</p>
+      <p>The RGB color values of your color are: {`(${r}, ${g}, ${b})`}</p>
+      <p>Choose another color to see their values.</p>
     </section>
   );
 }
