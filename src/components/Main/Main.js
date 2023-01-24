c69b8a17121a97f01332dc49355800c9c91bf55b
Michelle Ross <miross629@gmail.com>
2023-01-24 14:12:55 -0800

    Remove Nav from Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Nav from '../Nav/Nav.js';
 import Display from '../Display/Display.js';
 
 export default function Main() {
   return (
     <>
-      {/* <Nav /> */}
       <Display />
     </>
   );
